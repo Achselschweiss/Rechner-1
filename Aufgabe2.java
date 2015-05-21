468a461451291bc3802848703a74055fb36a8111
Thomas Müller <Achselschweiss@users.noreply.github.com>
2015-05-21 13:54:13 +0200

    Update Aufgabe2.java

--- Aufgabe2.java
+++ Aufgabe2.java
@@ -11,15 +11,15 @@ public class Aufgabe2 {
 		int keepRunning = 1;
 		
 		while (keepRunning == 1){
-			System.out.println("Hallo ich bin Programm 700.0");
+			System.out.println("Hallo ich bin Programm Missgeburt 2.0");
 			
-			System.out.println("Gib die erste Zahl ein.");
+			System.out.println("Gib die erste Nutte ein.");
 				int a = s.nextInt();
 			
-			System.out.println("Gib den Operator ein.");
+			System.out.println("Gib den Zuhälter ein.");
 				String operator = s.next();
 			
-			System.out.println("Gib die zweite Zahl ein.");
+			System.out.println("Gib die zweite Nutte ein.");
 				int b = s.nextInt();
 			
 			double result = 0;
@@ -46,11 +46,11 @@ public class Aufgabe2 {
 					System.out.println("Diesen Operator " + operator + "kenne ich nicht!");
 					result = 0;
 			}
-			String ausgabe = "Ergebnis von " + a + " " + operator + " " + b + " = " + result;
+			String ausgabe = "Der Fick von " + a + " " + operator + " " + b + " = " + result;
 			
 			System.out.println(ausgabe);
 			
-			System.out.println("Zum weiter rechnen 1 drücken.");
+			System.out.println("Zum weiter rechnen 1 drücken, du hurensohn.");
 			keepRunning = s.nextInt();
 			
 		}
